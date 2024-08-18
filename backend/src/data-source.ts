cf46e1503f04ed21d5b3a784f1b7b703153e4e32
João Neto <134328180+Joao-neto-01@users.noreply.github.com>
2024-08-18 19:29:48 -0300

    Update data-source.ts

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import { User } from "./models/user";
+import { User } from "./entities/user";
 
 export const AppDataSource = new DataSource({
   type: "sqlite",
