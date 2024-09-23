9d8e12ea04147ee6bebeee1fe94da53449df3483
João Neto <134328180+Joao-neto-01@users.noreply.github.com>
2024-09-23 18:13:51 -0300

    Atualização Frontend/RegisterView

--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -2,16 +2,70 @@
 import Header from '@/components/Header.vue';
 import Footer from '@/components/Footer.vue';
 
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import { api } from '@/api'
+import { AxiosError } from 'axios'
+import type { User, Role } from '@/types'
+import { useUserStore } from '@/stores/userStore'
+import { useRoute, useRouter } from 'vue-router'
+
+const user = ref({
+    role: {
+        name: ""
+    }
+} as User);
 
 const showEyeIcon = ref(false)
 const showPassword = ref(false)
+
+const loading = ref(false);
+const register_filled = ref(true);
+const password_filled = ref(true);
+
+const router = useRouter();
+
+const error_message = ref('');
 
 function toggleEyeIcon() {
     showEyeIcon.value = true;
 }
 function togglePasswordVisibility() {
     showPassword.value = !showPassword.value;
+}
+
+
+async function createUser() {
+    try {
+        loading.value = true
+
+        const res = await api.post(`/users/`, {
+            name: user.value.name,
+            username: user.value.username,
+            email: user.value.email,
+            password: user.value.password,
+            role: "Usuário padrão",
+            profile_picture_Url: ""
+        },
+        );
+        user.value = res.data.dados;
+
+        router.push('/login');
+
+
+        //console.log(user.value);
+
+        /*if(userStore.userData)
+            userStore.authenticaded(user.value, userStore.jwt);
+
+        userUpdated.value = true*/
+
+    } catch(error) {
+        if(error instanceof AxiosError)
+            error_message.value = error.response?.data.erro.mensagem;
+
+    } finally {
+        loading.value = false;
+    }
 }
 </script>
 
@@ -26,36 +80,73 @@ function togglePasswordVisibility() {
         
         <div class="registration-container">
             <div class="registration-box">
-                <h1>Cadastro</h1>
-                <label>
-                    Email
-                    <input type="text" placeholder="Digite seu email">
-                </label>
-                <label>
-                    Nome de usuário
-                    <input type="text" placeholder="Digite seu nome de usuário">
-                </label>
-                <label>
-                    Senha
-                    <div class="password-field">
-                        <input @input="toggleEyeIcon"
-                            :type="showPassword ? 'text' : 'password'"
-                            placeholder="Digite sua senha">
-                        <span v-if="showEyeIcon" class="toggle-password" @click="togglePasswordVisibility">
-                            <template v-if="showPassword">
-                                <img src="@/assets/eye-svgrepo-com.svg" alt="Olho aberto" width="25" height="25">
-                            </template>
-                            <template v-else>
-                                <img src="@/assets/eye-closed-svgrepo-com.svg" alt="Olho fechado" width="25" height="25">
-                            </template>
-                        </span>
-                    </div>
-                </label>
-                <div class="checkbox-container">
-                <input type="checkbox" id="age-confirmation">
-                <label for="age-confirmation" class="checkbox-label">Você confirma ter mais de 16 anos e que possivelmente estará vendendo sua alma e aceita os Termos de Uso</label>
+
+                <div v-if="error_message !== ''" class="alert-danger" role="alert">
+                    {{ error_message }}
                 </div>
-                <button class="submit-button">Cadastre-se</button>
+
+
+                <form v-if="!loading" class="profile-info" @submit.prevent="createUser">
+                    <h1>Cadastro</h1>
+                    <label>
+                        Nome
+                        <input  v-model="user.name" 
+                        type="text" 
+                        placeholder="Digite seu Nome">  
+                        <div v-if="!register_filled" class="invalid-feedback">
+                        — email ou nome de usuário — é um campo obrigatório.
+                        </div>  
+                    </label>
+
+                    
+                    <label>
+                        Email
+                        <input  v-model="user.email" 
+                        type="text" 
+                        placeholder="Digite seu email">
+                        <div v-if="!register_filled" class="invalid-feedback">
+                        email — é um campo obrigatório.
+                        </div> 
+                    </label>
+                    <label>
+                        Nome de usuário
+                        <input v-model="user.username" 
+                        type="text" 
+                        placeholder="Digite seu nome de usuário">
+                        <div v-if="!register_filled" class="invalid-feedback">
+                        nome de usuário — é um campo obrigatório.
+                        </div>
+                    </label>
+                    <label>
+                        Senha
+                        <div class="password-field">
+                            <input v-model="user.password" @input="toggleEyeIcon"
+                                :type="showPassword ? 'text' : 'password'"
+                                placeholder="Digite sua senha">
+                            <span v-if="showEyeIcon" class="toggle-password" @click="togglePasswordVisibility">
+                                <template v-if="showPassword">
+                                    <img src="@/assets/eye-svgrepo-com.svg" alt="Olho aberto" width="25" height="25">
+                                </template>
+                                <template v-else>
+                                    <img src="@/assets/eye-closed-svgrepo-com.svg" alt="Olho fechado" width="25" height="25">
+                                </template>
+                            </span>
+                            <div v-if="!password_filled" class="invalid-feedback">
+                                A senha é um campo obrigatório.
+                            </div>
+                        </div>
+                    </label>
+                    <div class="checkbox-container">
+                        <input type="checkbox" id="age-confirmation">
+                        <label for="age-confirmation" class="checkbox-label">Você confirma ter mais de 16 anos e aceita os Termos de Uso</label>
+                    </div>
+                    <input class="submit-button" type="submit" value="Cadastre-se"/>
+
+                </form>
+                <p v-else class="alert-loading">
+                Esperando resposta do servidor.
+                </p>
+
             </div>
 
             <Footer />
@@ -167,6 +258,11 @@ function togglePasswordVisibility() {
     cursor: pointer;
     border: none;
     transition: background 0.3s;
+
+    margin-top: 30px;
+
+    margin-left: 130px;
+
 
 }
 
